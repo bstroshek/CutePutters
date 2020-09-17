49b0dd38369edf3bdc1d89657fb880a3b3c18973
bstroshek <bstroshek@gmail.com>
2020-09-17 15:21:02 +0300

    hero-slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+(function($) {
+    $(document).ready(function(){
 
-	// Custom JS
+        $('.hero-slider').slick({
+            appendArrows: $('.hero-slider-arrows'),
+            prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
+            nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
+        });
 
-});
+
+    });
+})(jQuery);
