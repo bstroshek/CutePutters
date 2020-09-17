dfad5aed34618086ab12a684a6be816db221a0ca
bstroshek <bstroshek@gmail.com>
2020-09-17 17:25:30 +0300

    hero-slider-script-fix-slick

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 
         $('.hero-slider').slick({
             infinite: true,
-            slidesToShow: 3,
+            slidesToShow: 1,
             appendArrows: $('.hero-slider-arrows'),
             prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
             nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
