01103b2d9fce27ec142e138aae512a25fe3460e1
bstroshek <bstroshek@gmail.com>
2020-09-17 17:02:42 +0300

    hero-slider-script-fix4

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,28 @@
-(function($) {
-    $(document).ready(function(){
+// (function($) {
+//     $(document).ready(function(){
+//
+//         $('.hero-slider').slick({
+//             appendArrows: $('.hero-slider-arrows'),
+//             prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
+//             nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
+//         });
+//
+//
+//     });
+//
+// })(jQuery);
 
-        $('.hero-slider').slick({
-            appendArrows: $('.hero-slider-arrows'),
-            prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
-            nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
-        });
 
 
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    $('.hero-slider').slick({
+        appendArrows: $('.hero-slider-arrows'),
+        prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
+        nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
     });
-})(jQuery);
+
+
+});
+});
