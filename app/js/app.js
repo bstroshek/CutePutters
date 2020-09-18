a39257f7c9a2d2bb4ce3316f4dea5fb6de060c26
bstroshek <bstroshek@gmail.com>
2020-09-19 02:14:39 +0300

    desktop

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 
         // HOME PAGE hero tabs
         $('.tabgroup > div').addClass('hide');
-        $('.tabgroup > div:first-of-type').removeClass('hide');
+        $('.tabgroup > div:nth-of-type(2)').removeClass('hide');
         $('.tabs a').click(function(e){
             e.preventDefault();
             var $this = $(this),
@@ -46,6 +46,38 @@
             slidesToShow: 1,
             dots: false,
             appendArrows: $('.js-rx-slider-arrows'),
+            prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
+            nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
+        });
+
+
+
+
+
+        // HOME PAGE Cure section tabs
+
+        $('.cure-tabgroup > div').hide();
+        $('.cure-tabgroup > div:nth-of-type(2)').show();
+        $('.cure-tabs a').click(function(e){
+            e.preventDefault();
+            var $this = $(this),
+                tabgroup = '#'+$this.parents('.cure-tabs').data('tabgroup'),
+                others = $this.closest('li').siblings().children('a'),
+                target = $this.attr('href');
+            others.removeClass('active');
+            $this.addClass('active');
+            $(tabgroup).children('div').hide();
+            $(target).show();
+
+        });
+
+
+        // HOME PAGE Favorite section slider
+        $('.favorite__slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            appendArrows: $('.favorite-slider-arrows'),
             prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
             nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
         });
