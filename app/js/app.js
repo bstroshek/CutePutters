571b61422fae3ecb117cf26a40d2f4cda6800f59
bstroshek <bstroshek@gmail.com>
2020-09-22 19:09:20 +0300

    collection page hero

--- app/js/app.js
+++ app/js/app.js
@@ -109,6 +109,23 @@
 
 
 
+        // COLLECTION PAGE hero tabs
+
+
+        $('.collection-tabgroup > div').hide();
+        $('.collection-tabgroup > div:first-of-type').show();
+        $('.tabs a').click(function(e){
+            e.preventDefault();
+            var $this = $(this),
+                tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+                others = $this.closest('li').siblings().children('a'),
+                target = $this.attr('href');
+            others.removeClass('active');
+            $this.addClass('active');
+            $(tabgroup).children('div').hide();
+            $(target).show();
+
+        });
 
 
 
