2c130bde20ae7d41aefd1ac9f06ae85f2821d5f2
bstroshek <bstroshek@gmail.com>
2020-09-29 14:14:47 +0300

    Home Page - favorite sections mobile

--- app/js/app.js
+++ app/js/app.js
@@ -114,7 +114,15 @@
             dots: true,
             appendArrows: $('.favorite-slider-arrows'),
             prevArrow:'<span class="hero-slider-left"><img src="images/dest/hero-slider-left.png" alt="icon" width="50" height="100"></span>',
-            nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>'
+            nextArrow:'<span class="hero-slider-right"><img src="images/dest/hero-slider-right.png" alt="icon" width="50" height="100"></span>',
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        dots: false
+                    }
+                }
+            ]
         });
 
 
